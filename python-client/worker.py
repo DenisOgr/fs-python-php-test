1f39297d0f1f54b5e62511a5363984d6340580de
dmitrij <heetap@yandex.ru>
2018-03-12 11:51:35 +0200

    add changes

--- python-client/worker.py
+++ python-client/worker.py
@@ -11,14 +11,15 @@ def get_elastic():
 
 
 def get_user_documents(elastic, user_id, index, doc_type, q):
-    result = elastic.search(index=index, doc_type=doc_type, body={"query": {"match": {'user_id':user_id}}}, size=env.DEFAULT_SIZE)
-    documents = np.array([])
+    query = {
+        'query': {
+            'match': {
+                'user_id': user_id
+            }
+        }
+    }
 
-    if result['hits']['total'] == 0:
-        q.put(documents)
-        return True
-
-    documents = np.array([hit['_source']['form_id'] for hit in result['hits']['hits']])
+    documents = _get_documents(elastic, index, doc_type, query, 'form_id')
 
     q.put(documents)
 
@@ -45,15 +46,8 @@ def get_user_documents_by_title(elastic, user_id, title, index, doc_type,q):
             }
         },
     }
-    result = elastic.search(index=index, doc_type=doc_type, body=query, size=env.DEFAULT_SIZE)
 
-    documents = np.array([])
-
-    if result['hits']['total'] == 0:
-        q.put(documents)
-        return True
-
-    documents = np.array([hit['_source']['project_id'] for hit in result['hits']['hits']])
+    documents = _get_documents(elastic, index, doc_type, query, 'project_id')
 
     q.put(documents)
 
@@ -81,16 +75,7 @@ def get_user_form_content(elastic, form_ids, title, index, doc_type):
         },
     }
 
-    result = elastic.search(index=index, doc_type=doc_type, body=query, size=env.DEFAULT_SIZE)
-
-    documents = np.array([])
-
-    if result['hits']['total'] == 0:
-        return documents
-
-    documents = np.array([hit['_source']['form_id'] for hit in result['hits']['hits']])
-
-    return documents
+    return _get_documents(elastic, index, doc_type, query, 'form_id')
 
 
 def get_user_projects_by_forms(elastic, form_ids, user_id, index, doc_type):
@@ -113,6 +98,101 @@ def get_user_projects_by_forms(elastic, form_ids, user_id, index, doc_type):
         },
     }
 
+    return _get_documents(elastic, index, doc_type, query, 'project_id')
+
+
+def get_public_documents(elastic, index, doc_type, queue, user_id, query, organization_id):
+    query = {
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'match': {
+                            'organization_id': organization_id
+                        }
+                    },
+                    {
+                        'more_like_this': {
+                            'fields': ['title'],
+                            'like': [query],
+                            'min_term_freq': 1,
+                            'min_doc_freq': 1
+                        },
+                    }
+                ],
+                'must_not': [
+                    {
+                        'term': {
+                            'user_id': user_id
+                        }
+                    }
+                ]
+            }
+        },
+    }
+
+    documents = _get_documents(elastic, index, doc_type, query, 'project_id')
+
+    queue.put(documents)
+
+
+def get_user_comments(elastic, index, doc_type, queue, user_id, query):
+    query = {
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'match': {
+                            'user_id': user_id
+                        }
+                    },
+                    {
+                        'more_like_this': {
+                            'fields': ['comment'],
+                            'like': [query],
+                            'min_term_freq': 1,
+                            'min_doc_freq': 1
+                        },
+                    }
+                ]
+            }
+        },
+    }
+
+    results= _get_documents(elastic, index, doc_type, query, False, True)
+
+    queue.put(results)
+
+
+def get_user_chats(elastic, index, doc_type, queue, user_id, query):
+    query = {
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'match': {
+                            'user_id': user_id
+                        }
+                    },
+                    {
+                        'more_like_this': {
+                            'fields': ['comment'],
+                            'like': [query],
+                            'min_term_freq': 1,
+                            'min_doc_freq': 1
+                        },
+                    }
+                ]
+            }
+        },
+    }
+
+    results= _get_documents(elastic, index, doc_type, query, False, True)
+
+    queue.put(results)
+
+
+def _get_documents(elastic, index, doc_type, query, filter_field, get_hits=False):
     result = elastic.search(index=index, doc_type=doc_type, body=query, size=env.DEFAULT_SIZE)
 
     documents = np.array([])
@@ -120,30 +200,39 @@ def get_user_projects_by_forms(elastic, form_ids, user_id, index, doc_type):
     if result['hits']['total'] == 0:
         return documents
 
-    documents = np.array([hit['_source']['project_id'] for hit in result['hits']['hits']])
+    if get_hits:
+        return result['hits']['hits']
+
+    documents = np.array([hit['_source'][filter_field] for hit in result['hits']['hits']])
 
     return documents
 
 
-esastic = get_elastic()
-title = 'meet'
-user_id = 30
+def process(title, user_id, organization_id):
+    esastic = get_elastic()
 
-documents_by_title_queue = Queue()
-documents_by_title_process = Process(target=get_user_documents_by_title, args=(esastic,user_id,title, 'documents','doc',documents_by_title_queue,))
-documents_by_title_process.start()
-documents_by_title_process.join()
-documents_by_title = documents_by_title_queue.get()
+    documents_by_title_queue = Queue()
+    documents_by_title_process = Process(target=get_user_documents_by_title,
+                                         args=(esastic, user_id, title, 'documents', 'doc', documents_by_title_queue,))
+    documents_by_title_process.start()
+    documents_by_title_process.join()
+    documents_by_title = documents_by_title_queue.get()
 
-documents_queue = Queue()
-documents_process = Process(target=get_user_documents, args=(esastic, user_id, 'documents','doc',documents_queue,))
-documents_process.start()
-documents_process.join()
-documents = documents_queue.get()
+    documents_queue = Queue()
+    documents_process = Process(target=get_user_documents,
+                                args=(esastic, user_id, 'documents', 'doc', documents_queue,))
+    documents_process.start()
+    documents_process.join()
+    documents = documents_queue.get()
 
-user_form_content = get_user_form_content(esastic, documents, title, 'form_content', 'doc')
-user_projects_by_content = get_user_projects_by_forms(esastic, user_form_content, user_id, 'documents','doc')
+    user_form_content = get_user_form_content(esastic, documents, title, 'form_content', 'doc')
+    user_projects_by_content = get_user_projects_by_forms(esastic, user_form_content, user_id, 'documents', 'doc')
 
-result = np.unique(np.concatenate([user_projects_by_content, documents_by_title]))
+    projects = np.unique(np.concatenate([user_projects_by_content, documents_by_title]))
 
-print(json.dumps(result.tolist()))
+    return {
+        'projects': projects
+    }
+
+
+result = process('meet', 30, 100)
